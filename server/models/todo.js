fb0372974a66c086be001ff2d52e91eba983fda5
Nadja Speidel <nadja.speidel@gmx.de>
2018-08-29 22:47:32 -0400

    Add GET /todos route

--- server/models/todo.js
+++ server/models/todo.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Todo = mongoose.model('Todo', {             //create Mongoose Model
   text : {
     type: String,
-    required: true,                              //value must exist
+    required: false,                              //value must exist
     minlenth: 1,                                 //empty string not allowed
-    trim: true                                   //delete space beginning or ending of value
+    trim: true,                                  //delete space beginning or ending of value
   },
   completed: {
     type: Boolean,
