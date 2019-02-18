3c49645e8667a7dab64d4858b6dfcf18b432d463
Nadja Speidel <nadja.speidel@gmx.de>
2019-02-18 14:57:18 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,20 +2,21 @@
 
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require ('bcryptjs');
 
+var password = '123abc';
 
-var data = {
-  id: 10
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
+var hashedPassword = '$2a$10$EqNQTXf9lO00GgZKOmf5FOxN7wZCTzzHVK2ods1rXhNtmuAzbgXFO';
 
-var token = jwt.sign(data, '123abc');
-
-console.log(token);
-
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded', decoded);
-
+bcrypt.compare('123', hashedPassword, (err, res) => {
+  console.log(res);
+});
 
 
 
