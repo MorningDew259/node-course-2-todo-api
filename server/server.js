7882c63f7bfa1ec52d8b917477c92c289ac277ba
Nadja Speidel <nadja.speidel@gmx.de>
2019-04-22 21:08:12 -0400

    Pushing current status

--- server/server.js
+++ server/server.js
@@ -111,13 +111,27 @@ app.post('/users', (req, res) => {
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
-    res.status(400).send();
+    res.status(400).send(e);
   })
 });
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+//app.delete('/users/me/token', )
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
